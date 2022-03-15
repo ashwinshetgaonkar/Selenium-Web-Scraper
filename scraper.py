e9c161acfa563f258ac530b14ab3cadbc1e08193
Ashwin Shetgaonkar <75084722+ashwinshetgaonkar@users.noreply.github.com>
2022-03-15 08:20:12 +0000

    second commit

--- scraper.py
+++ scraper.py
@@ -1,1 +1,27 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+YOUTUBE_TRENDING_URL='https://www.youtube.com/feed/trending'
+
+response=requests.get(YOUTUBE_TRENDING_URL)  #does not execute javascript
+
+if response.status_code == 200:
+  print("We are good to go")
+else:
+  print("Error")
+
+
+# print(f"Output:{response.text[:100]}" )
+# with open('trending.html','w') as f:
+#   f.write(response.text)
+
+soup=BeautifulSoup(response.text,'html.parser')
+
+# print('Page title:',soup.title.text)
+
+# find video divs
+selective_class='style-scope ytd-rich-grid-media'
+video_divs=soup.find_all('div',{'class':selective_class})
+
+print("videos found:",len(video_divs))
+# print("video tag:",video_divs[0])
+
